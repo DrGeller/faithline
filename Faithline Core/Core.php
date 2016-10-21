b9226564d0c4ffd2d436ae5e205f77f9076c4a4a
Dr Geller <rodneygeller@outlook.com>
2016-10-21 09:09:26 +0800

    update 2016.10.21

---
+++ Faithline Core/Core.php
@@ -0,0 +1,16 @@
+<?php
+	header('Content-Type:text/html; charset=utf8');
+	$progressStatus = $_POST[progressStatus];
+
+	
+
+	if($progressStatus == 'init') {
+		
+	} else if($progressStatus == 'signin') {
+		
+	}	else if($progressStatus == 'signup') {
+		
+	} else if($progressStatus == 'signout') {
+		
+	}
+?>
